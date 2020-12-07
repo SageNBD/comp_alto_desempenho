89db75d80f4e2ac276864790389f92520ad8ad1d
Abner Santos <abner.santos@usp.br>
2020-12-07 15:00:40 -0300

    Finished this god forsaken assignment

--- MenoresQueMediaMatriz-MPI-OMP/menoresmediaparallel.c
+++ MenoresQueMediaMatriz-MPI-OMP/menoresmediaparallel.c
@@ -58,21 +58,22 @@ int  main(int argc, char *argv[])
         }
         
         fscanf(inputfile, "%d\n", &dim); //Lê a dimensão de MAT
-        
-        //Aloca a matriz
-        MAT = (int *)malloc(dim * dim * sizeof(int));
+    }
 
+    //Aloca a matriz
+    MAT = (int *)malloc(dim * dim * sizeof(int));
+        
+    if (myrank == 0)
+	{
         //Aloca um vetor para armazenar a quantide de nrs menores que a media por coluna
         vet_menor = (int *)malloc(dim * sizeof(int));
-        vet_menor_buffer = (int *)malloc(dim * sizeof(int));
-        
+
         // incializa com zero as posicoes de vet_menor
         for (j = 0; j< dim; j++)
         {
-            vet_menor_buffer[j] = 0;
+            vet_menor[j] = 0;
         }
 
-        media_MAT = 0;
         //Lê a matriz MAT
         for(i=0;i<dim;i++)
         {
@@ -80,37 +81,90 @@ int  main(int argc, char *argv[])
             {
                 // Transpomos a matriz na leitura para facilitar a divisão entre os processos
                 fscanf(inputfile, "%d ", &(MAT[j*dim+i]));
-                media_MAT += MAT[j*dim+i];
             }
         }
-
-        media_MAT = media_MAT / (double)(dim*dim);
 
         // fecha o arquivo de entrada
         fclose(inputfile);
 	}
 
     MPI_Bcast(&dim, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&media_MAT, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     int partitionSize = (dim/npes);
     int start = partitionSize * myrank * dim;
 	int end = (myrank != npes - 1 ? start + partitionSize * dim : dim * dim);
 
-    int sendcounts[npes], displacements[npes];
+    int sendcounts[npes], displacements_scatter[npes];
     for(int i = 0; i < (npes-1); i++)
     {
         sendcounts[i] = partitionSize * dim;
-        displacements[i] = partitionSize * dim * i;
+        displacements_scatter[i] = partitionSize * dim * i;
     }
     sendcounts[npes-1] = dim*dim - (partitionSize * dim * (npes-1));
-    displacements[npes-1] = dim * partitionSize * (npes-1);
+    displacements_scatter[npes-1] = dim * partitionSize * (npes-1);
 
     int cols[end-start];
 
-    MPI_Scatterv(MAT, sendcounts, displacements, MPI_INT, cols, end-start, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scatterv(MAT, sendcounts, displacements_scatter, MPI_INT, cols, end-start, MPI_INT, 0, MPI_COMM_WORLD);
+
+    int sum_local = 0;
+    #pragma omp parallel
+    {
+        #pragma omp for
+        for (int j = 0; j < end-start; j++)
+        { 
+            #pragma omp atomic
+            sum_local += cols[j];
+        }
+    }
+
+    int recvcounts[npes], displacements_gather[npes];
+    int* sums;
+    if(myrank == 0)
+    {
+        sums = (int *)malloc(npes * sizeof(int));
+
+        for(int i = 0; i < npes; i++)
+        {
+            recvcounts[i] = 1;
+            displacements_gather[i] = i;
+        }
+    }
+
+    // Faz o Gather.
+    MPI_Gatherv(&sum_local, 1, MPI_INT, sums, recvcounts, displacements_gather, MPI_INT, 0, MPI_COMM_WORLD);
+
+    media_MAT = 0;
+    if(myrank == 0)
+    {
+        for(int i = 0; i < npes; i++)
+            media_MAT += sums[i];
+
+        media_MAT = media_MAT / (double)(dim*dim);
+    }
+
+
+    MPI_Bcast(&media_MAT, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    for(int i = 0; i < (npes-1); i++)
+    {
+        sendcounts[i] = partitionSize * dim;
+        displacements_scatter[i] = partitionSize * dim * i;
+    }
+    sendcounts[npes-1] = dim*dim - (partitionSize * dim * (npes-1));
+    displacements_scatter[npes-1] = dim * partitionSize * (npes-1);
+
+    MPI_Scatterv(MAT, sendcounts, displacements_scatter, MPI_INT, cols, end-start, MPI_INT, 0, MPI_COMM_WORLD);
 
     int nOfCols = (end - start) / dim;
+
+    vet_menor_buffer = (int *)malloc(nOfCols * sizeof(int));
+
+    // incializa com zero as posicoes de vet_menor
+    for (j = 0; j< nOfCols; j++)
+    {
+        vet_menor_buffer[j] = 0;
+    }
 
     for (int i = 0; i < nOfCols; i++)   
     {     
@@ -122,30 +176,30 @@ int  main(int argc, char *argv[])
                 if (cols[i*dim+j] < media_MAT)
                 {
                     #pragma omp atomic
-                    vet_menor_buffer[myrank * partitionSize + i]++;
+                    vet_menor_buffer[i]++;
                 }
             }
         }
     }
 
-    int recvCounts[npes], displs[npes];
-    for(int i = 0; i < npes - 1; i++)
+    if(myrank == 0)
     {
-        recvCounts[i] = partitionSize;
-        displs[i] = i * partitionSize;
+        for(int i = 0; i < npes - 1; i++)
+        {
+            recvcounts[i] = partitionSize;
+            displacements_gather[i] = i * partitionSize;
+        }
+        recvcounts[npes-1] = dim - ((npes-1) * partitionSize);
+        displacements_gather[npes-1] = partitionSize * (npes-1);
     }
-    recvCounts[npes-1] = dim - (npes-1) * partitionSize;
-    displs[npes-1] = partitionSize * (npes-1);
 
-
-    printf("aaaa");
-    MPI_Gatherv(vet_menor, dim, MPI_INT, vet_menor_buffer, recvCounts, displs, MPI_INT, 0, MPI_COMM_WORLD);
-    printf("bbbb");
+    MPI_Gatherv(vet_menor_buffer, nOfCols, MPI_INT, vet_menor, recvcounts, displacements_gather, MPI_INT, 0, MPI_COMM_WORLD);
 
     if (myrank == 0)
     {
         for (int i = 0; i < dim; i++)
             printf("%d ", vet_menor[i]);
+        printf("\n");
     }
 
 	MPI_Finalize();
